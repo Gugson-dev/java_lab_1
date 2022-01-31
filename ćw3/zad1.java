22fe77823eb03145200c49518352bba34f00862e
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-01-31 22:59:43 +0100

    Zrobione
    Poprawiono tabulacje

--- ćw3/zad1.java
+++ ćw3/zad1.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
-public class zad1 {
-
-    public static void main(String[] args) {
+public class zad1 
+{
+    public static void main(String[] args) 
+    {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Podaj imie: ");
--- ćw3/zad2.java
+++ ćw3/zad2.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
-public class zad2 {
-    public static void main(String[] args) {
+public class zad2 
+{
+    public static void main(String[] args) 
+    {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Podaj liczbe A: ");
