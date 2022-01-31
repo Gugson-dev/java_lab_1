2e30b53c841598cb17c23efce6afee251482030b
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-01-31 23:00:35 +0100

    Zrobione
    Poprawiono tabulacje

--- ćw4/Main.java
+++ ćw4/Main.java
@@ -1,8 +1,9 @@
 import java.util.Random;
 
-public class Main {
-
-    public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
